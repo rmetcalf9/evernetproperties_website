c34a59ffe2a3a219abf5ea0388f62a57a83acd2d
Robert <rmetcalf9@googlemail.com>
2023-12-09 12:21:00 +0000

    Getting header right

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,19 +1,26 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar class="main-toolbar">
-        <div class="fit column no-wrap justify-start items-center content-start">
-          <q-toolbar-title>
-            Evernet Properties Limited
-          </q-toolbar-title>
-          <q-toolbar-subtitle>
-            We find properties
-          </q-toolbar-subtitle>
+      <q-toolbar class="main-toolbar column no-wrap justify-start items-center content-start">
+        <div class="row no-wrap justify-start items-center content-start">
+          <img
+            class="main-logo"
+            alt="Evernet properties logo"
+            src="~assets/main_logo.svg"
+          >
+          <div class="column no-wrap justify-start items-center content-start">
+            <q-toolbar-title>
+              Evernet Properties Limited
+            </q-toolbar-title>
+            <q-toolbar-subtitle>
+              We find properties
+            </q-toolbar-subtitle>
+          </div>
         </div>
       </q-toolbar>
       <div class="sub-toolbar fit row no-wrap justify-center items-center content-start">
         <div v-for="item in menu_items" :key="item.name">
-          <div class="sub-toolbar-item">{{ item.name }}</div>
+          <router-link :to="item.target">{{ item.name }}</router-link>
         </div>
       </div>
     </q-header>
@@ -32,8 +39,8 @@ export default defineComponent({
   data () {
     return {
       menu_items: [
-        {name: 'Home'},
-        {name: 'About'}
+        {name: 'Home', target: '/'},
+        {name: 'About', target: '/about'}
       ]
     }
   }
@@ -41,6 +48,14 @@ export default defineComponent({
 </script>
 
 <style>
+.main-logo {
+  width: 80px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  padding-left: 10px;
+  padding-right: 10px;
+  margin: 0px;
+}
 .main-toolbar {
   color: white;
   background: grey;
@@ -60,5 +75,10 @@ export default defineComponent({
 .sub-toolbar-item {
   padding-left: 5px;
   padding-right: 5px;
+}
+a {
+  text-decoration: none;
+  color: white;
+  padding: 5px;
 }
 </style>
