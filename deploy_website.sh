bdbd85d2ffe70015013ba8ad8ff46b6a57ffeae9
Robert <rmetcalf9@googlemail.com>
2023-12-09 12:57:38 +0000

    Upd

--- deploy_website.sh
+++ deploy_website.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-###git push origin `git subtree split --prefix build_folder master`:gh-pages --force
 ##git subtree pull --prefix=dist/spa origin gh-pages (If change made upstream)
 
 quasar build
+git push origin `git subtree split --prefix build_folder master`:gh-pages --force
 git subtree push --prefix dist/spa origin gh-pages
