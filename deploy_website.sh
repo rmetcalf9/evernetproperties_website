28daab62eb536a1be08d7326db1e22a8a834025c
Robert <rmetcalf9@googlemail.com>
2023-12-09 12:45:57 +0000

    Clear dist

--- deploy_website.sh
+++ deploy_website.sh
@@ -2,4 +2,4 @@
 
 ###git push origin `git subtree split --prefix build_folder master`:gh-pages --force
 ##git subtree pull --prefix=dist/spa origin gh-pages (If change made upstream)
-###git subtree push --prefix dist/spa origin gh-pages
+git subtree push --prefix dist/spa origin gh-pages
