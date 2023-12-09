6ea3d80a2613c9710094cc306acd3754fb5cb4d9
Robert <rmetcalf9@googlemail.com>
2023-12-09 12:52:03 +0000

    Up

--- deploy_website.sh
+++ deploy_website.sh
@@ -2,4 +2,6 @@
 
 ###git push origin `git subtree split --prefix build_folder master`:gh-pages --force
 ##git subtree pull --prefix=dist/spa origin gh-pages (If change made upstream)
+
+quasar build
 git subtree push --prefix dist/spa origin gh-pages
--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -4,7 +4,7 @@
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >About
+    >About2
   </q-page>
 </template>
 
