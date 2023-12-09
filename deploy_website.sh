445ab5e5a94596fff5b50719c218f61d635cef51
Robert <rmetcalf9@googlemail.com>
2023-12-09 13:03:46 +0000

    New website version

--- deploy_website.sh
+++ deploy_website.sh
@@ -3,5 +3,8 @@
 ##git subtree pull --prefix=dist/spa origin gh-pages (If change made upstream)
 
 quasar build
+git add --all
+git commit -m"New website version"
+git push
 git push origin `git subtree split --prefix dist/spa main`:gh-pages --force
 git subtree push --prefix dist/spa origin gh-pages
