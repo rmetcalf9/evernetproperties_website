6cf8b512e69f08e9618e129d8bf8ddf8c7908c26
Robert <rmetcalf9@googlemail.com>
2023-12-09 10:32:52 +0000

    Header created

---
+++ deploy_website.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git subtree push --prefix dist/spa origin gh-pages
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,43 +1,22 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+      <q-toolbar class="main-toolbar">
+        <div class="fit column no-wrap justify-start items-center content-start">
+          <q-toolbar-title>
+            Evernet Properties Limited
+          </q-toolbar-title>
+          <q-toolbar-subtitle>
+            We find properties
+          </q-toolbar-subtitle>
+        </div>
       </q-toolbar>
+      <div class="sub-toolbar fit row no-wrap justify-center items-center content-start">
+        <div v-for="item in menu_items" :key="item.name">
+          <div class="sub-toolbar-item">{{ item.name }}</div>
+        </div>
+      </div>
     </q-header>
-
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -47,70 +26,39 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-]
 
 export default defineComponent({
   name: 'MainLayout',
-
-  components: {
-    EssentialLink
-  },
-
-  setup () {
-    const leftDrawerOpen = ref(false)
-
+  data () {
     return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      menu_items: [
+        {name: 'Home'},
+        {name: 'About'}
+      ]
     }
   }
 })
 </script>
+
+<style>
+.main-toolbar {
+  color: white;
+  background: grey;
+  align-items: center;
+  padding-top: 20px;
+  padding-bottom: 10px;
+  text-align: center;
+}
+.sub-toolbar {
+  color: white;
+  background: darkgrey;
+  align-items: center;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  text-align: center;
+}
+.sub-toolbar-item {
+  padding-left: 5px;
+  padding-right: 5px;
+}
+</style>
