b280e0dd9823c68d99f8369cd6b834c0b259fbb7
Robert <rmetcalf9@googlemail.com>
2023-12-09 12:37:28 +0000

    Test

--- deploy_website.sh
+++ deploy_website.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ###git push origin `git subtree split --prefix build_folder master`:gh-pages --force
+##git subtree pull --prefix=dist/spa origin gh-pages (If change made upstream)
 git subtree push --prefix dist/spa origin gh-pages
