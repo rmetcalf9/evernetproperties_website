8f736d39faee2b871a47e5f1cabc41d1df92abef
Robert <rmetcalf9@googlemail.com>
2024-06-05 12:27:33 +0100

    New website version

--- dist/spa/assets/brand-colors.f64cc965.js
+++
@@ -1,1 +0,0 @@
-import{b as r,s as a}from"./index.e8cf8d05.js";var o=r(()=>{a("primary","grey")});export{o as default};
--- dist/spa/assets/notify-defaults.78515d0c.js
+++
@@ -1,1 +0,0 @@
-import{b as o,N as t}from"./index.e8cf8d05.js";var s=o(async()=>{t.setDefaults({timeout:2e3,actions:[{icon:"close",color:"white"}]})});export{s as default};
