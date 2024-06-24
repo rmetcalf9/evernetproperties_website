8b67264af26229479a8adf10daf671facd0bc902
Robert <rmetcalf9@googlemail.com>
2024-06-24 23:24:59 +0100

    New website version

--- dist/spa/assets/brand-colors.d5194ae3.js
+++
@@ -1,1 +0,0 @@
-import{b as r,s as a}from"./index.e57f4910.js";var o=r(()=>{a("primary","grey")});export{o as default};
---
+++ dist/spa/assets/googleapi.d2acf569.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.6c8b5959.js";var g=a(({app:o})=>{window.gapi.load("client"),o.config.globalProperties.$gapi=window.gapi});export{g as default};
