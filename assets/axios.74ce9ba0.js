b92f9f5ebfefe1cbd1d1f76164dfe9ec3dd10900
Robert <rmetcalf9@googlemail.com>
2024-07-02 15:58:04 +0100

    New website version

--- assets/axios.74ce9ba0.js
+++
@@ -1,1 +0,0 @@
-import{b as i}from"./index.cd103895.js";import{a}from"./axios.6e1fcf85.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
---
+++ assets/brand-colors.e675eef0.js
@@ -0,0 +1,1 @@
+import{b as r,s as a}from"./index.4a74404e.js";var o=r(()=>{a("primary","grey")});export{o as default};
