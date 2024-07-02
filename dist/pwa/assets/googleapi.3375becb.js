df3a4d35637c694f14601f22504e9a6cb3d522d0
Robert <rmetcalf9@googlemail.com>
2024-07-02 15:58:04 +0100

    New website version

---
+++ dist/pwa/assets/googleapi.3375becb.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.4a74404e.js";var g=a(({app:o})=>{window.gapi.load("client"),o.config.globalProperties.$gapi=window.gapi});export{g as default};
--- dist/pwa/assets/googleapi.3ae77603.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.cd103895.js";var g=a(({app:o})=>{window.gapi.load("client"),o.config.globalProperties.$gapi=window.gapi});export{g as default};
