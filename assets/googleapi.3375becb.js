3cdbc00e4f27024871fe69c9b4de74efcec13718
Robert <rmetcalf9@googlemail.com>
2024-07-03 22:16:26 +0100

    New website version

--- assets/googleapi.3375becb.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./index.4a74404e.js";var g=a(({app:o})=>{window.gapi.load("client"),o.config.globalProperties.$gapi=window.gapi});export{g as default};
---
+++ assets/googleapi.425a6f06.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index.007b0c12.js";var g=a(({app:o})=>{window.gapi.load("client"),o.config.globalProperties.$gapi=window.gapi});export{g as default};
