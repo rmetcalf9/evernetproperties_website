4e18233834ba1d140f1d27b9181bc1dca3a6d048
Robert <rmetcalf9@googlemail.com>
2024-07-10 21:01:02 +0100

    Drawing main stages of workflow

--- src/components/Workflow/Chart.vue
+++ src/components/Workflow/Chart.vue
@@ -10,6 +10,110 @@ import * as d3 from 'd3'
 
 import Workflow from './FirstWorkflow.js'
 
+function draw_workflow(parent, workflow_json) {
+  const context = {
+    items_drawn: [],
+    cols: [-500, 0, 500]
+  }
+  draw_stage_and_all_children(
+    parent,
+    workflow_json.initial_stage,
+    workflow_json.stages[workflow_json.initial_stage],
+    context,
+    workflow_json,
+    200,
+    false
+  )
+
+}
+
+function draw_stage_and_all_children(parent, this_stage_id, stage_data, context, workflow_json, cur_y, is_fail_path) {
+  if (context.items_drawn.includes(this_stage_id)) {
+    return //alredy drawn
+  }
+  let draw_col = 0
+  if (is_fail_path) {
+    draw_col = 2
+  } else {
+    if (typeof (stage_data.draw_col) !== 'undefined') {
+      draw_col = stage_data.draw_col
+    }
+  }
+  const group = parent.append('g')
+    .attr('transform', 'translate( ' + context.cols[draw_col] + ' ' + cur_y + ')')
+
+  context.items_drawn.push(this_stage_id)
+
+  draw_stage(group, stage_data, is_fail_path)
+  if (typeof (stage_data.progression) !== 'undefined') {
+    if (typeof (stage_data.progression.failed) !== 'undefined') {
+      draw_stage_and_all_children(
+        parent,
+        stage_data.progression.failed,
+        workflow_json.stages[stage_data.progression.failed],
+        context,
+        workflow_json,
+        cur_y + 150,
+        true
+      )
+    }
+    if (typeof (stage_data.progression.success) !== 'undefined') {
+      stage_data.progression.success.forEach(function (ite) {
+        draw_stage_and_all_children(
+          parent, ite.next_stage,
+          workflow_json.stages[ite.next_stage],
+          context,
+          workflow_json,
+          cur_y + 300,
+          false
+        )
+      })
+    }
+
+
+  }
+}
+
+function draw_stage(parent, stage_data, is_fail_stage) {
+  // 0,0 is centre TOP of the rectangle
+  const group = parent.append('g')
+
+  const width = 400
+
+  let colour = 'lightgreen'
+  if (is_fail_stage) {
+    colour = 'pink'
+  }
+
+  group
+    .append('rect')
+    .attr('width', width)
+    .attr('height', 130)
+    .attr('x', -(width/2))
+    .attr('y', 0)
+    .attr('style', 'fill: ' + colour + ';stroke-width:3;stroke:rgb(0,0,0)')
+
+    group
+      .append('text')
+      .attr('x', 0)
+      .attr('y', 50)
+      .attr('text-anchor', 'middle')
+      .style("font-size", "28px")
+      .style("font-weight", "500")
+      .text(stage_data.name)
+
+      group
+        .append('text')
+        .attr('x', 0)
+        .attr('y', 110)
+        .attr('text-anchor', 'middle')
+        .style("font-size", "56px")
+        .style("font-weight", "500")
+        .text('0')
+
+
+}
+
 export default defineComponent({
   name: 'WorkflowChart',
   data () {
@@ -19,10 +123,10 @@ export default defineComponent({
       svg: undefined,
       background_item_group: undefined,
       chartarea: {
-        xmin: -650,
-        xmax: 650,
+        xmin: -950,
+        xmax: 950,
         ymin: -150,
-        ymax: 800
+        ymax: 2300
       }
     }
   },
@@ -86,6 +190,8 @@ export default defineComponent({
       }
       // console.log('nodeData:', this.nodeData)
       xdiv.appendChild(chart(this))
+
+      draw_workflow(this.allzoomedelements, this.workflow)
     },
     clickDIV () {
       // console.log('CLICK')
--- src/components/Workflow/FirstWorkflow.js
+++ src/components/Workflow/FirstWorkflow.js
@@ -3,5 +3,79 @@
 
 export default {
   "id": 1,
-  "name": 'BRR Project Workflow'
+  "name": 'BRR Project Workflow',
+  "initial_stage": "1",
+  "stages": {
+    "1": {
+      "name": "Lead",
+      "progression": {
+        "failed": "1.1",
+        "success": [
+          {
+            "next_stage": "2"
+          }
+        ]
+      },
+    },
+    "1.1": { "name": "Rejected Lead"},
+    "2": {
+      "name": "Potential",
+      "progression": {
+        "failed": "2.1",
+        "success": [
+          {
+            "next_stage": "3"
+          }
+        ]
+      },
+    },
+    "2.1": { "name": "Rejected Lead"},
+    "3": {
+      "name": "Viewing Booked",
+      "progression": {
+        "success": [
+          {
+            "next_stage": "4"
+          }
+        ]
+      }
+    },
+    "4": {
+      "name": "Viewed",
+      "progression": {
+        "failed": "4.1",
+        "success": [
+          {
+            "next_stage": "5"
+          }
+        ]
+      },
+    },
+    "4.1": { "name": "Unviable"},
+    "5": {
+      "name": "Offer made",
+      "progression": {
+        "success": [
+          {
+            "next_stage": "6"
+          },
+          {
+            "next_stage": "7"
+          }
+        ]
+      },
+    },
+    "6": {
+      "name": "Rejected offer",
+      "draw_col": 1, // Defaults to 0 - unless fail stage defaults to 2
+      "progression": {
+        "success": [
+          {
+            "next_stage": "7"
+          }
+        ]
+      },
+    },
+    "7": { "name": "Accepted offer"},
+  }
 }
