2d4d728e63e3be4f5c0336b5a0c620e7f421956b
Robert <rmetcalf9@googlemail.com>
2024-07-29 10:43:04 +0100

    Refactored out types

--- src/components/CommonCalcComponents/ActivityLog.vue
+++ src/components/CommonCalcComponents/ActivityLog.vue
@@ -27,19 +27,39 @@
 <script>
 import { defineComponent } from 'vue'
 
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
+    (+c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> +c / 4).toString(16)
+  );
+}
+
+function get_new_activity_item (type, text) {
+  return {
+    id: uuidv4(),
+    type: type,
+    timestamp: (new Date()).toISOString(),
+    text: text
+  }
+}
+
+function tmp_get_example_activity_log_data () {
+  let ret_var = []
+
+  ret_var.push(get_new_activity_item('book_viewing', 'This is some text for the book_viewing'))
+  ret_var.push(get_new_activity_item('hold_viewing', 'This is some text for the hold_viewing'))
+  ret_var.push(get_new_activity_item('call_agent', 'This is some text for the call agent'))
+  ret_var.push(get_new_activity_item('wf_move', 'Moved Stage'))
+  ret_var.push(get_new_activity_item('offer_made', 'Offer made'))
+  ret_var.push(get_new_activity_item('offer_rejected', 'Offer rejected'))
+  ret_var.push(get_new_activity_item('offer_accepted', 'Offer accepted'))
+
+  return ret_var
+}
+
 export default defineComponent({
   name: 'BrrCalcActivityJob',
   data () {
     return {
-      tests: [
-        {t: 'book_viewing', a: 'Book a viewing', b: 'event'},
-        {t: 'hold_viewing', a: 'Hold a viewing', b: 'visibility'},
-        {t: 'call_agent', a: 'Call with selling agent', b: 'call'},
-        {t: 'wf_move', a: 'Move to different workflow stage', b: 'donut_large'},
-        {t: 'offer_made', a: 'Make an offer', b: 'attach_money'},
-        {t: 'offer_rejected', a: 'Offer rejected', b: 'money_off'},
-        {t: 'offer_accepted', a: 'Offer accepted', b: 'paid'},
-      ]
     }
   },
   computed: {
@@ -50,22 +70,34 @@ export default defineComponent({
     },
     activity_log_display () {
       let ret_var = []
+      let activity_log = tmp_get_example_activity_log_data()
       ret_var = ret_var.concat([{
         id: '011234',
-        label: 'LABEL'
+        label: 'Not done date display'
       }])
-      ret_var = ret_var.concat(this.tests.map(function (x) {
-        return {
-          id: x.t,
+
+      const type_data_map = {
+        book_viewing: { sent: true, name: 'Viewing booked' },
+        hold_viewing: { sent: false, name: 'Viewing' },
+        call_agent: { sent: true, name: 'Call to agent' },
+        wf_move: { sent: false, name: 'Workflow' },
+        offer_made: { sent: true, name: 'Offer made' },
+        offer_rejected: { sent: false, name: 'Offer rejected' },
+        offer_accepted: {sent: false, name: 'Offer accepted' },
+      }
+
+      activity_log.forEach(function (ite) {
+        ret_var = ret_var.concat({
+          id: ite.t,
           label: undefined,
-          name: 'TODO',
-          text: x.a,
-          sent: true,
+          name: type_data_map[ite.type].name,
+          text: ite.text,
+          sent: type_data_map[ite.type].sent,
           stamp: '1 minute ago',
-          avatar: 'src/assets/activity_log_icon_' + x.t + '.png'
-        }
-      }))
-      console.log('aaa', ret_var)
+          avatar: 'src/assets/activity_log_icon_' + ite.type + '.png'
+        })
+      })
+
       return ret_var
     }
   },
