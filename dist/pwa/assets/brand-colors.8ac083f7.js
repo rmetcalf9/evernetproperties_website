a292be05b4f208c1ec50d3b54d0f0de50fc5861a
Robert <rmetcalf9@googlemail.com>
2024-08-11 20:54:32 +0100

    New website version

---
+++ dist/pwa/assets/brand-colors.8ac083f7.js
@@ -0,0 +1,1 @@
+import{b as r,s as a}from"./index.2eeb48d0.js";var o=r(()=>{a("primary","grey")});export{o as default};
---
+++ dist/pwa/assets/notify-defaults.16a0309c.js
@@ -0,0 +1,1 @@
+import{b as o,N as t}from"./index.2eeb48d0.js";var s=o(async()=>{t.setDefaults({timeout:2e3,actions:[{icon:"close",color:"white"}]})});export{s as default};
