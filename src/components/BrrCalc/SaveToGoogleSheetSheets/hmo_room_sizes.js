be9963a617dc86355c4ff94e854c1a13d233c08d
Robert <rmetcalf9@googlemail.com>
2024-08-20 09:57:47 +0100

    HMO Room sizes complete

--- src/components/BrrCalc/SaveToGoogleSheetSheets/hmo_room_sizes.js
+++ src/components/BrrCalc/SaveToGoogleSheetSheets/hmo_room_sizes.js
@@ -11,8 +11,86 @@ function get_sheet_values (spreadsheet, vueobj, sheet_id_map) {
     value_requests: [],
     misc_data: {}
   }
+  const column_widths = [130,	100,	58,	58,	100, 200, 200]
+  let i = 0;
+  while (i < column_widths.length) {
+    context.requests.push(su.adjustcolumnwidth(i,column_widths[i]))
+    i++;
+  }
 
   hmo_utils.header(context, sheet_name, vueobj, su, 'List of room sizes to check against licence standards')
+
+  context.cur_row = context.cur_row + 1
+  context.value_requests.push({
+    range: sheet_name + '!A' + context.cur_row.toString() + ':A' + context.cur_row.toString(),
+    values: [['Total area (sqm)']]
+  })
+
+  context.requests.push(su.makeboldandvaligntop(context.cur_row-1,context.cur_row,0,1))
+
+  context.cur_row = context.cur_row + 1
+  const total_row = context.cur_row
+  context.value_requests.push({
+    range: sheet_name + '!A' + context.cur_row.toString() + ':G' + context.cur_row.toString(),
+    values: [[
+      'Level',
+      'Room on plan',
+      'x',
+      'y',
+      'sqm',
+      'Reconfig Use',
+      'Notes'
+    ]]
+  })
+  context.requests.push(su.makeboldandvaligntop(context.cur_row-1,context.cur_row,0,7))
+
+  const template_data = [
+    ['Ground', 'Kitchen', '-1', '-1'],
+    ['Ground', 'Dinning Room', '-1', '-1'],
+    ['Bedroom', 'Dinning Room', '-1', '-1'],
+    ['', '', '2', ''],
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', ''],
+    ['', '', '', '']
+  ]
+  template_data.forEach(function (row) {
+    context.cur_row = context.cur_row + 1
+    context.value_requests.push({
+      range: sheet_name + '!A' + context.cur_row.toString() + ':G' + context.cur_row.toString(),
+      values: [[
+        row[0],
+        row[1],
+        row[2],
+        row[3],
+        '=C' + ( context.cur_row).toString() + '*D' + ( context.cur_row).toString(),
+        '',
+        ''
+      ]]
+    })
+  })
+  context.cur_row = context.cur_row + 1
+
+  context.cur_row = context.cur_row + 1
+  context.value_requests.push({
+    range: sheet_name + '!A' + context.cur_row.toString() + ':G' + context.cur_row.toString(),
+    values: [[
+      '',
+      '',
+      '',
+      'Total',
+      '=sum(E' + (total_row+1).toString() + ':E' + (context.cur_row-1).toString() + ')',
+      '',
+      ''
+    ]]
+  })
+  context.requests.push(su.makeboldandvaligntop(context.cur_row-1,context.cur_row,0,7))
+
 
 
   return {
