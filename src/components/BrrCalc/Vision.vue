ee6c1a0454ba97b308cb54ad90f2ee006ac22da5
Robert <rmetcalf9@googlemail.com>
2024-08-20 07:52:31 +0100

    Added strategies to vision

--- src/components/BrrCalc/Vision.vue
+++ src/components/BrrCalc/Vision.vue
@@ -6,7 +6,10 @@
         <q-input v-model="devplan" label="What is the development plan?" class="col-grow" />
         <q-btn round  color="primary" icon="info" @click="helpdevplan" />
       </div>
-
+      &nbsp;
+      <div class="text-h6">Strategies</div>
+      <div><q-checkbox v-model="strategy.flip" label="Flip" @click="setchanged"/></div>
+      <div><q-checkbox v-model="strategy.hmo" label="HMO" @click="setchanged"/></div>
     </q-card-section>
   </q-card>
 </template>
@@ -14,30 +17,42 @@
 <script>
 import { defineComponent } from 'vue'
 
+function get_default_strategy () {
+  return {
+    flip: false,
+    hmo: false
+  }
+}
+
 export default defineComponent({
   name: 'BrrCalcVision',
   emits: ['projectchanged'],
   data () {
     return {
       devplan: '',
+      strategy: get_default_strategy(),
       emit_project_change_notification: true
     }
   },
   computed: {
     serializer_card_data () {
       return {
-        devplan: this.devplan
+        devplan: this.devplan,
+        strategy: this.strategy
       }
     }
   },
   watch: {
     serializer_card_data(val) {
-      if (this.emit_project_change_notification) {
-        this.$emit('projectchanged', 'Vision')
-      }
+      this.setchanged()
     }
   },
   methods: {
+    setchanged () {
+      if (this.emit_project_change_notification) {
+        this.$emit('projectchanged', 'Vision')
+      }
+    },
     helpdevplan () {
       this.$q.dialog({
         title: 'What is the development plan?',
@@ -49,6 +64,12 @@ export default defineComponent({
     serializer_load_data (data_to_load) {
       this.emit_project_change_notification = false
       this.devplan = data_to_load.devplan
+      if (typeof (data_to_load.strategy) === 'undefined') {
+        this.strategy = get_default_strategy()
+      } else {
+        this.strategy.flip = data_to_load.strategy.flip
+        this.strategy.hmo = data_to_load.strategy.hmo
+      }
 
       const TTT = this
       setTimeout(function () {
