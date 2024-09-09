3dce3351ec638bc709288202eebe6d18c3ed2325
Robert <rmetcalf9@googlemail.com>
2024-09-09 12:21:08 +0100

    New website version

--- dist/pwa/assets/brand-colors.33c6c47e.js
+++
@@ -1,1 +0,0 @@
-import{b as r,s as a}from"./index.d28e8c48.js";var o=r(()=>{a("primary","grey")});export{o as default};
---
+++ dist/pwa/assets/notify-defaults.752aba03.js
@@ -0,0 +1,1 @@
+import{b as o,N as t}from"./index.7e0a52fc.js";var s=o(async()=>{t.setDefaults({timeout:2e3,actions:[{icon:"close",color:"white"}]})});export{s as default};
