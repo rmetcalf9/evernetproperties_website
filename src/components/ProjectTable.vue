af1dd9d4d533c3eafed4852fe7d87a574c2d7913
Robert <rmetcalf9@googlemail.com>
2024-09-25 16:13:31 +0100

    Adding selling agent and fixing sort issue

--- src/components/ProjectTable.vue
+++ src/components/ProjectTable.vue
@@ -4,14 +4,14 @@
       <q-table
         flat bordered
         title="Projects"
-        :rows="projects"
+        :rows="table_content"
         :columns="columns"
         :filter="filter"
         :pagination="initialPagination"
         no-data-label="I didn't find anything for you"
         no-results-label="The filter didn't uncover any results"
         row-key="name"
-      >
+      ><!--
         <template v-slot:body-cell-address="props">
           <q-td @click="onRowClick(props.row)">
             <div v-if="!props.row.loaded">
@@ -22,38 +22,20 @@
             </div>
           </q-td>
         </template>
-
-        <template v-slot:body-cell-workflowstage="props">
-          <q-td @click="onRowClick(props.row)">
-            <div v-if="props.row.loaded">
-              <div v-if="typeof (props.row.item.workflow) !== 'undefined'">
-                {{ getWorkflowStage(props.row.item.workflow).name }}
-              </div>
-            </div>
-          </q-td>
-        </template>
+-->
 
         <template v-slot:body-cell-visionandnotes="props">
           <q-td @click="onRowClick(props.row)">
             <div v-if="props.row.loaded">
-              <div class="projecttablehead">{{ props.row.item.sub_section_details.vision.devplan }}</div>
-              <div v-if="typeof (props.row.item.sub_section_details.dealbasicinfo.notes) !== 'undefined'">
-                <div v-for="line_item in props.row.item.sub_section_details.dealbasicinfo.notes.split('\n')" :key='line_item'>
+              <div class="projecttablehead">{{ props.row.devplan }}</div>
+              <div v-if="typeof (props.row.notes) !== 'undefined'">
+                <div v-for="line_item in props.row.notes.split('\n')" :key='line_item'>
                   {{ line_item }}
                 </div>
               </div>
             </div>
           </q-td>
         </template>
-
-        <template v-slot:body-cell-source="props">
-          <q-td @click="onRowClick(props.row)">
-            <div v-if="props.row.loaded">
-              <div>{{ get_source_text(props.row) }}</div>
-            </div>
-          </q-td>
-        </template>
-
 
         <template v-slot:top-right>
           <q-input borderless dense debounce="300" v-model="filter" clearable placeholder="Search">
@@ -119,24 +101,32 @@ export default defineComponent({
           required: true,
           label: 'Stage',
           align: 'left',
-          field: 'address',
-          sortable: true
+          field: 'stage',
+          sortable: false
         },
         {
           name: 'visionandnotes',
           required: true,
           label: 'Vision and Notes',
           align: 'left',
-          field: 'address',
+          field: 'vandnotes',
+          sortable: false
+        },
+        {
+          name: 'source',
+          required: true,
+          label: 'Source',
+          align: 'left',
+          field: 'source',
           sortable: true
-          },
-          {
-            name: 'source',
-            required: true,
-            label: 'Source',
-            align: 'left',
-            field: 'address',
-            sortable: true
+        },
+        {
+          name: 'agent',
+          required: true,
+          label: 'Agent',
+          align: 'left',
+          field: 'selling_agent',
+          sortable: true
         }
       ],
       initialPagination: {
@@ -150,6 +140,34 @@ export default defineComponent({
   computed: {
     user_profile () {
       return this.backend_connection_store.user_profile
+    },
+    table_content () {
+      const TTT = this
+      return this.projects.map(function (project) {
+        if (!project.loaded) {
+          return {
+            loaded: false,
+            address: 'Loading...',
+            source: '',
+            selling_agent: '',
+            vandnotes: '',
+            devplan: '',
+            notes: '',
+            stage: ''
+          }
+        }
+        console.log('p', project.item.sub_section_details)
+        return {
+          loaded: project.loaded,
+          address: project.item.sub_section_details.dealbasicinfo.address,
+          source: TTT.get_source_text(project),
+          selling_agent: project.item.sub_section_details.dealbasicinfo.selling_agent,
+          vandnotes: 'NOT DISPLAYED',
+          devplan: project.item.sub_section_details.vision.devplan,
+          notes: project.item.sub_section_details.dealbasicinfo.notes,
+          stage: TTT.getWorkflowStage(project.item.workflow).name
+        }
+      })
     }
   },
   methods: {
