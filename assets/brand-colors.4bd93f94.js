f42a79d491d9126fa760cf48ca8f50efc626ebdb
Robert <rmetcalf9@googlemail.com>
2024-12-07 22:28:22 +0000

    New website version

---
+++ assets/brand-colors.4bd93f94.js
@@ -0,0 +1,1 @@
+import{b as r,s as a}from"./index.b0f494b0.js";var o=r(()=>{a("primary","grey")});export{o as default};
---
+++ assets/notify-defaults.42ea045d.js
@@ -0,0 +1,1 @@
+import{b as o,N as t}from"./index.b0f494b0.js";var s=o(async()=>{t.setDefaults({timeout:2e3,actions:[{icon:"close",color:"white"}]})});export{s as default};
