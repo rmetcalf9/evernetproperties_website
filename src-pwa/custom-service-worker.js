6b2fd23e1dbb88192727e2314b1b34fb2bd63f9a
Robert <rmetcalf9@googlemail.com>
2024-12-07 10:42:34 +0000

    Bug fix in no cache argument.

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -33,7 +33,7 @@ if (process.env.MODE !== 'ssr' || process.env.PROD) {
           new RegExp('/ppbevent.html'),
           new RegExp('/simpleroi.html'),
           new RegExp('/investorcrm.html'),
-          new RegExp('^/nc/$')
+          new RegExp('^/nc/')
       ] }
     )
   )
