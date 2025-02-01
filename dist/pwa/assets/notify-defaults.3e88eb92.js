37f54d9ee5b8470dd1e912b318f02913833f349c
Robert <rmetcalf9@googlemail.com>
2025-02-01 18:13:58 +0000

    New website version

---
+++ dist/pwa/assets/notify-defaults.3e88eb92.js
@@ -0,0 +1,1 @@
+import{b as o,N as t}from"./index.1f7450dc.js";var s=o(async()=>{t.setDefaults({timeout:2e3,actions:[{icon:"close",color:"white"}]})});export{s as default};
--- dist/pwa/assets/use-quasar.706b31da.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bd as r}from"./index.d2ef3ad6.js";function u(){return a(r)}export{u};
