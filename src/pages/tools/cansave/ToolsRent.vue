293044b9d710d1012889008e8e4cad7932fb2ff6
Robert <rmetcalf9@googlemail.com>
2025-04-23 16:56:26 +0100

    Fixing order

--- src/pages/tools/cansave/ToolsRent.vue
+++ src/pages/tools/cansave/ToolsRent.vue
@@ -7,6 +7,27 @@
     <div>This is the rental projects menu. This contains the functionality around the rental property strategies - Rent to HMO, Rent to Serviced Accommodation, etc. These functions allow you to enter leads, make calls to landlords and manage the viewing calendar.</div>
     <div class="row">
       <q-card
+        inline class="q-ma-sm card-style toolsrent-tool-card" @click="$router.push('/tools/rentproject/enterlead')">
+        <q-card-section>
+          <div class="text-h6">Enter Rental Lead</div>
+          <div class="row">
+            <div>
+              <img
+                alt="Item Picture"
+                src="~assets/enter_rental_lead.png"
+                class="toolsrent-tool-picture"
+              >
+            </div>
+            <div class="col q-ma-sm">
+              <div style="height: 150px;">Enter a lead for the rent to SA or rent to HMO strategy. A lead is a landlord you want to approach to find out if they are interested in renting their property. Leads can be obtained by looking at rental adverts on gumtree, spareroom, facebook, or any other platform that enables you to get in contact with landlords with rooms to rent.</div>
+              <div align="right">
+                <q-btn round  color="primary" icon="info" />
+              </div>
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+      <q-card
         inline class="q-ma-sm card-style toolsrent-tool-card" @click="$router.push('/tools/rentproject/callleads')"
       >
         <q-card-section>
@@ -21,27 +42,6 @@
             </div>
             <div class="col q-ma-sm">
               <div style="height: 150px;">You have leads to call! Make calls to generate new rent-to-rent opportunities</div>
-              <div align="right">
-                <q-btn round  color="primary" icon="info" />
-              </div>
-            </div>
-          </div>
-        </q-card-section>
-      </q-card>
-      <q-card
-        inline class="q-ma-sm card-style toolsrent-tool-card" @click="$router.push('/tools/rentproject/enterlead')">
-        <q-card-section>
-          <div class="text-h6">Enter Rental Lead</div>
-          <div class="row">
-            <div>
-              <img
-                alt="Item Picture"
-                src="~assets/enter_rental_lead.png"
-                class="toolsrent-tool-picture"
-              >
-            </div>
-            <div class="col q-ma-sm">
-              <div style="height: 150px;">Enter a lead for the rent to SA or rent to HMO strategy. A lead is a landlord you want to approach to find out if they are interested in renting their property. Leads can be obtained by looking at rental adverts on gumtree, spareroom, facebook, or any other platform that enables you to get in contact with landlords with rooms to rent.</div>
               <div align="right">
                 <q-btn round  color="primary" icon="info" />
               </div>
