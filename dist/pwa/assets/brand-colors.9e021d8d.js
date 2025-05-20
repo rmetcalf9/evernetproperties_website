959974c3564e5fe89f2fecabbecf6e4258c10164
Robert <rmetcalf9@googlemail.com>
2025-05-20 12:59:43 +0100

    New website version

---
+++ dist/pwa/assets/brand-colors.9e021d8d.js
@@ -0,0 +1,1 @@
+import{b as r,s as a}from"./index.461ae6f5.js";var o=r(()=>{a("primary","grey")});export{o as default};
--- src/rjmversion.js
+++ src/rjmversion.js
@@ -1,2 +1,2 @@
 /* eslint-disable */
-export default { codebasever: '0.0.21' }
+export default { codebasever: '0.0.22' }
