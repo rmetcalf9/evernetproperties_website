7a1b30f9d55d092d9216f332ae1f25e8c6b31556
Robert <rmetcalf9@googlemail.com>
2025-07-23 18:50:00 +0100

    Adding qr code #107

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -17,6 +17,23 @@
       </div>
     </div>
   </div>
+  <div>
+    <div class="flex-break"></div>
+    <div class="flex row first-section">
+      <div>
+        <h3 style="margin: 0px;">Share link</h3>
+        <p>Scan this link on your phone to get to this site</p>
+        </div>
+      <div style="max-width: 400px;">
+        <img
+          alt="QR Code"
+          src="~assets/EvernetpropertiesQrCode.png"
+          class="qrcode"
+        >
+        <center><a href="https://www.evernetproperties.com">https://www.evernetproperties.com</a></center>
+      </div>
+    </div>
+  </div>
   </q-page>
 </template>
 
@@ -33,5 +50,11 @@ export default defineComponent({
   width: 200px;
   height: 200px;
   padding: 20px;
+}
+.qrcode {
+  width: 300px;
+  height: 300px;
+  padding-left: 10px;
+  padding-right: 10px;
 }
 </style>
