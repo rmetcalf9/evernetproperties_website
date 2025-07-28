7669a2b7fba5562be2e36d7ccea9e42c9ef65a1f
Robert <rmetcalf9@googlemail.com>
2025-07-28 09:53:40 +0100

    New website version

--- dist/pwa/assets/QSpace.5c8bb129.js
+++
@@ -1,1 +0,0 @@
-import{q as a,g as e}from"./index.472238a3.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
--- src/rjmversion.js
+++ src/rjmversion.js
@@ -1,2 +1,2 @@
 /* eslint-disable */
-export default { codebasever: '0.0.40' }
+export default { codebasever: '0.0.41' }
