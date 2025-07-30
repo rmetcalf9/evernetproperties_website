7a62fece62a73fcfe5ad03a78f9f391cfcd4b824
Robert <rmetcalf9@googlemail.com>
2025-07-30 11:14:05 +0100

    New website version

--- dist/pwa/assets/notify-defaults.5dd852ec.js
+++
@@ -1,1 +0,0 @@
-import{b as o,N as t}from"./index.4e3fd5ab.js";var s=o(async()=>{t.setDefaults({timeout:2e3,actions:[{icon:"close",color:"white"}]})});export{s as default};
--- src/rjmversion.js
+++ src/rjmversion.js
@@ -1,2 +1,2 @@
 /* eslint-disable */
-export default { codebasever: '0.0.42' }
+export default { codebasever: '0.0.43' }
