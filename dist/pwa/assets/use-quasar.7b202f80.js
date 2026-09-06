e66cbefd84a952ec81de71d421c78967b608d785
Robert <rmetcalf9@googlemail.com>
2026-09-06 20:04:03 +0100

    New website version

--- dist/pwa/assets/use-quasar.7b202f80.js
+++
@@ -1,1 +0,0 @@
-import{i as a,bi as r}from"./index.ce16cc00.js";function u(){return a(r)}export{u};
--- src/rjmversion.js
+++ src/rjmversion.js
@@ -1,2 +1,2 @@
 /* eslint-disable */
-export default { codebasever: '0.0.58' }
+export default { codebasever: '0.0.59' }
